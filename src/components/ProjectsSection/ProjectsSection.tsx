ba910b1ade93560619356d71dbd5dc6d087eeca4
Chris Ware <c.p.ware@hotmail.com>
2025-01-08 23:50:05 -0700

    Featured product done

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -1,6 +1,36 @@
 import LayoutWrapper from "../LayoutWrapper";
 import SectionHeading from "../SectionHeading/SectionHeading";
 import styles from "./ProjectsSection.module.css";
+import Button from "../Button/Button";
+
+const data = [
+  {
+    id: 1,
+    title: "Tech Stack",
+    description: "Next.js 15, React 19, CSS Modules, Three.js, Sanity.io ",
+  },
+  {
+    id: 2,
+    title: "Font(s)",
+    description: '"Inter Tight" via Googl Fonts API',
+  },
+  {
+    id: 3,
+    title: "Color Scheme",
+    description:
+      "black: #1e1738, blue: #162f35, white: #ffffff, pink: #fcd2cd, purple: #bdb7d5, gray: #787a7d, lightGray: #aab9c5 ",
+  },
+  {
+    id: 4,
+    title: "Analytics",
+    description: "Plausible",
+  },
+  {
+    id: 4,
+    title: "Hosting",
+    description: "Vercel",
+  },
+];
 
 const ProjectsSection = () => {
   return (
@@ -9,6 +39,24 @@ const ProjectsSection = () => {
         <div className={styles.content}>
           <div className={styles.top}>
             <SectionHeading title='Projects' color='black' />
+            <h2 className={styles.heading}>Featured Project</h2>
+            <p className={styles.copy}>chrisware.dev - Portfolio website</p>
+          </div>
+          <div className={styles.bottom}>
+            <div className={styles.bottomLeft}></div>
+            <div className={styles.bottomRight}>
+              {data.map((x) => (
+                <div key={x.id} className={styles.section}>
+                  <h3 className={styles.title}>{x.title}</h3>
+                  <p className={styles.description}>{x.description}</p>
+                </div>
+              ))}
+              <div className={styles.btnContainer}>
+                <div className={styles.btnContainer}>
+                  <Button text='More Details' btnType='primary' href='/' />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </LayoutWrapper>

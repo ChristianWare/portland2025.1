5afe964fbe88b13dcebee4fb908ff905630a2966
Chris Ware <c.p.ware@hotmail.com>
2025-01-11 11:26:15 -0700

    Sanity schema update

--- src/sanity/schemaTypes/post.ts
+++ src/sanity/schemaTypes/post.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { defineField } from "sanity";
 
 export default {
@@ -17,6 +18,40 @@ export default {
       description:
         "Make the description brief so the visitor knows what to expect in the post",
       type: "string",
+      validation: (rule) => rule.min(50).max(250).required(),
+    }),
+    defineField({
+      type: "reference",
+      to: [{ type: "author" }],
+      name: "author",
+      title: "Author",
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title" },
+    }),
+    defineField({
+      name: "image",
+      type: "image",
+      title: "Image",
+      options: { hotspot: true },
+    }),
+    defineField({
+      name: "content",
+      title: "Content",
+      type: "array",
+      validation: (rule) => rule.required().error("Required"),
+      of: [
+        {
+          type: "block",
+        },
+        {
+          type: "image",
+          fields: [{ type: "text", name: "alt", title: "Alt" }],
+        },
+      ],
     }),
   ],
 };

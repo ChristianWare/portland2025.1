cd28608daab610e3033bdb8ff0dd63cccf33fed8
Chris Ware <c.p.ware@hotmail.com>
2025-01-12 20:52:30 -0700

    Blog section update

--- src/components/BlogSection/BlogSection.tsx
+++ src/components/BlogSection/BlogSection.tsx
@@ -2,11 +2,13 @@ import styles from "./BlogSection.module.css";
 import BlogCard from "../BlogCard/BlogCard";
 import BlogCard2 from "../BlogCard2/BlogCard2";
 import Button from "../Button/Button";
+import SectionHeading2 from "../SectionHeading2/SectionHeading2";
 
 const BlogSection = () => {
   return (
     <section className={styles.container}>
-      <h2 className={styles.heading}>My Journal</h2>
+      <SectionHeading2 title='My Journal' />
+      {/* <h2 className={styles.heading}>My Journal</h2> */}
       <div className={styles.content}>
         <div className={styles.bottom}>
           <div className={styles.bTop}>

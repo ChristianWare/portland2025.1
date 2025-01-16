b65340a5368e3af42b693f087b8b139a1d49c220
Chris Ware <c.p.ware@hotmail.com>
2025-01-15 17:01:38 -0700

    project schema done

--- src/components/ProjectHero/ProjectHero.tsx
+++ src/components/ProjectHero/ProjectHero.tsx
@@ -22,22 +22,19 @@ export default function ProjectHero() {
               <span className={styles.heading2}>speak for themselves</span>.
             </h1>
             <p className={styles.copy}>
-              {/* Functional is a lightweight app for small teams to streamline
-              <span className={styles.span}> timesheets,</span> time off and
-              track
-              <span className={styles.span}> employee wellbeing,</span> all
-              inside the team chat. */}
-              Each of my projects are thoughtfully crafted to combine{" "}
+              My portfolio demonstrates a broad range of my abilities. Each of
+              my projects are thoughtfully crafted to combine{" "}
               <span className={styles.span}> innovative </span>
               design with <span className={styles.span}> seamless </span>{" "}
-              functionality, ensuring a memorable user experience. Every detail
+              functionality.
+              {/* functionality, ensuring a memorable user experience. Every detail
               is <span className={styles.span}> meticulously </span>
               executed to bring ideas to life in the most{" "}
-              <span className={styles.span}> impactful </span> way.
+              <span className={styles.span}> impactful </span> way. */}
             </p>
             <div className={styles.box}>
               <div className={styles.sceneContainer}>
-                <Cog useViewportScale={false} fixedScale={[1, 1, 1]} />
+                <Cog useViewportScale={false} fixedScale={[1.2, 1.2, 1.2]} />
               </div>
             </div>
           </div>
--- src/sanity/schemaTypes/project.ts
+++ src/sanity/schemaTypes/project.ts
@@ -7,26 +7,122 @@ export default {
   type: "document",
   fields: [
     defineField({
-      name: "title",
-      title: "title",
-      description: "This is your blog post title",
+      name: "name",
+      title: "Name of Project",
       type: "string",
+      validation: (rule) => rule.required().error("Required"),
+    }),
+    defineField({
+      name: "image",
+      type: "image",
+      title: "Preview Image",
+      options: { hotspot: true },
+      validation: (rule) => rule.required().error("Required"),
+    }),
+    defineField({
+      name: "description",
+      type: "text",
+      title: "Description of Product",
+      validation: (rule) => rule.required().error("Required"),
     }),
 
-    // defineField({
-    //   name: "content",
-    //   title: "Content",
-    //   type: "array",
-    //   validation: (rule) => rule.required().error("Required"),
-    //   of: [
-    //     {
-    //       type: "block",
-    //     },
-    //     {
-    //       type: "image",
-    //       fields: [{ type: "text", name: "alt", title: "Alt" }],
-    //     },
-    //   ],
-    // }),
+    defineField({
+      name: "tags",
+      type: "array",
+      title: "Tags list",
+      validation: (rule) => rule.required().error("Required"),
+      of: [
+        {
+          type: "block",
+        },
+        // {
+        //   type: "image",
+        //   fields: [{ type: "text", name: "alt", title: "Alt" }],
+        // },
+      ],
+    }),
+    defineField({
+      name: "imagedesign",
+      type: "image",
+      title: "Design System Image",
+      options: { hotspot: true },
+      validation: (rule) => rule.required().error("Required"),
+    }),
+    defineField({
+      name: "imagehome",
+      type: "image",
+      title: "Home Page Image",
+      options: { hotspot: true },
+      validation: (rule) => rule.required().error("Required"),
+    }),
+    defineField({
+      name: "image1",
+      type: "image",
+      title: "Additianl Image 1",
+      options: { hotspot: true },
+    }),
+    defineField({
+      name: "image2",
+      type: "image",
+      title: "Additianl Image 2",
+      options: { hotspot: true },
+    }),
+    defineField({
+      name: "image3",
+      type: "image",
+      title: "Additianl Image 3",
+      options: { hotspot: true },
+    }),
+    defineField({
+      name: "image4",
+      type: "image",
+      title: "Additianl Image 4",
+      options: { hotspot: true },
+    }),
+    defineField({
+      name: "image5",
+      type: "image",
+      title: "Additianl Image 5",
+      options: { hotspot: true },
+    }),
+    defineField({
+      name: "image6",
+      type: "image",
+      title: "Additianl Image 6",
+      options: { hotspot: true },
+    }),
+    defineField({
+      name: "outcome",
+      type: "array",
+      title: "Outcome",
+      validation: (rule) => rule.required().error("Required"),
+      of: [
+        {
+          type: "block",
+        },
+      ],
+    }),
+    defineField({
+      name: "keytakeaways",
+      type: "array",
+      title: "Key Takeaways",
+      validation: (rule) => rule.required().error("Required"),
+      of: [
+        {
+          type: "block",
+        },
+      ],
+    }),
+    defineField({
+      name: "conclusion",
+      type: "array",
+      title: "Conclusion",
+      validation: (rule) => rule.required().error("Required"),
+      of: [
+        {
+          type: "block",
+        },
+      ],
+    }),
   ],
 };
